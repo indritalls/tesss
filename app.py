0d5387ff1f2560f3dceb230be82dd03946cfc56c
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-27 16:46:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/tes")
+@app.route("/hello")
 def index():
-	flash("Ketikkan truth atau dare")
+	flash("what's your name?")
 	return render_template("index.html")
 
 @app.route("/truth", methods=['POST', 'GET'])
-def handle_message(message):
-	if (message=="truth"):
-		flash("Coba jujur")
-		return render_template("index.html")
+def truth():
+	if (request.form['name_input']=="truth"):
+		flash("cobalah jujur")
+	return render_template("index.html")
+
+@app.route("/dare", methods=['POST', 'GET'])
+def dare():
+	if (request.form['name_input']=="dare"):
+		flash("beranilah menerima tantangan ini")
+	return render_template("index.html")
