7c2dfea3c0c79324ca749d1d78a0379bcc575145
indritalls <95203855+indritalls@users.noreply.github.com>
2021-12-27 22:27:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash
+import random
 
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
@@ -10,10 +11,56 @@ def index():
 
 @app.route("/mulai", methods=['POST', 'GET'])
 def truth():
+	t = {'Kalau kamu bisa jadi tidak terlihat, apa hal pertama yang akan kamu lakukan?':1, 
+        'Apa rahasia yang kamu sembunyikan dari orangtuamu?':2,
+        'Siapa orang yang diam-diam kamu sukai?' :3,
+        'Siapa orang terakhir yang kamu kepoin di media sosial?':4,
+        'Kalau ada jin yang memberikanmu tiga permohonan, apa yang kamu inginkan?':5,
+        'Jika kamu kembali ke masa lalu, apa yang akan kamu lakukan?':6,
+        'Apa tontonan favoritmu saat masih kecil?': 7,
+        'Siapa orang yang paling sering kamu chat?':8,
+        'Apa kebohongan terbesar yang pernah kamu katakan kepada orangtuamu?':9,
+        'Apa mimpi paling aneh yang pernah kamu alami?':10,
+        'Ceritakan detail ciuman pertamamu…':11,
+        'Kapan terakhir kali kamu ngompol atau eek di celana?':12,
+        'Menurutmu, hewan apa yang terlihat paling mirip denganmu?':13,
+        'Di antara temanmu, siapa orang yang paling kamu suka dalam konteks romantis?':15,
+        'Di antara temanmu, siapa orang yang menurutmu paling baik dan paling buruk sifatnya?':16,
+        'Siapa mantan terindahmu?':16,
+        'Siapa orang yang ingin kamu jadikan istri/suami?':17,
+        'Apakah kamu pernah melakukan ghosting?':18,
+        'Apa aib yang kamu sembunyikan dari teman-temanmu?':19,
+        'Berapa jumlah mantanmu? sebutkan!':20,
+        }
+	tth = random.choice(list(t.keys()))
+	
+	d = {'Lakukan rap gaya bebas selama 3 menit!':1, 
+        'Biarkan orang lain membuat status menggunakan akun sosial mediamu!':2,
+        'Berikan ponselmu kepada salah satu di antara kita dan biarkan orang tersebut mengirim satu pesan kepada siapapun yang dia mau!' :3,
+        'Cium salah satu kaus kaki di antara temanmu!':4,
+        'Makan satu gigitan kulit pisang!':5,
+        'Peragakan salah satu orang di antara kita sampai ada yang bisa menebak siapa orang yang diperagakan!':6,
+        'Nyanyikanlah salah lagu lagu dari Rossa!': 7,
+        'Tirukan seorang selebriti sampai ada yang bisa menebak!':8,
+        'Bertingkahlah seperti Hotman Paris selama 2 menit!':9,
+        'Biarkan satu orang menggambar tato di wajahmu!':10,
+        'Tutuplah mata lalu raba muka salah satu di antara kita sampai kamu bisa menebak siapa orang itu!':11,
+        'Ungkapkan persaanmu kepada gebetanmu!':12,
+        'Push up 20 kali!':13,
+        'Kayang selama satu menit!':15,
+        'Plank selama satu menit!.':16,
+        'Coba teriak “aku sayang kamu” sekarang juga!':16,
+        'Baca dengan lantang pesan yang terakhir kali kamu kirim ke gebetanmu!':17,
+        'Telepon seorang teman dan katakan selamat ulang tahun sambil menyanyikan lagu!':18,
+        'Tunjukkan gerakan dance terbaikmu!':19,
+        'Parodikan adegan di film India kesukaanmu!':20,
+        }
+	dare = random.choice(list(d.keys()))
+
 	if (request.form['name_input']=="truth"):
-		flash("cobalah jujur")
+		flash(tth)
 		return render_template("index.html")
 
 	if (request.form['name_input']=="dare"):
-		flash("beranilah menerima tantangan ini")
+		flash(dare)
 		return render_template("index.html")
